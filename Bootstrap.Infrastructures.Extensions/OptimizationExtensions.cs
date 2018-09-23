90b48ac5b47c5e9693f97262353a80c953336fbb
Anobaka <anobaka@anobaka.com>
2018-09-23 12:51:08 +0800

    update

---
+++ Bootstrap.Infrastructures.Extensions/OptimizationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bootstrap.Infrastructures.Extensions
+{
+    public static class OptimizationExtensions
+    {
+        public static void OptimizeSingleAndListData<T, TData>(this T data, Func<T, TData> getSingleData,
+            Func<T, IEnumerable<TData>> getListData, Action<TData> setSingleData,
+            Action<IEnumerable<TData>> setListData) where TData : class
+        {
+            var single = getSingleData(data);
+            var list = getListData(data)?.Where(t => t != null && t != single).Distinct().ToList();
+            if (list?.Any() == true)
+            {
+                if (single != null)
+                {
+                    list.Add(single);
+                    single = null;
+                }
+                else
+                {
+                    if (list.Count == 1)
+                    {
+                        single = list.FirstOrDefault();
+                        list = null;
+                    }
+                }
+            }
+
+            setSingleData(single);
+            setListData(list);
+        }
+    }
+}
