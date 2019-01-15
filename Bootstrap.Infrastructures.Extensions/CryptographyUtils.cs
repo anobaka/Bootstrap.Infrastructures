fa30f840ac9831ee167cd4ea9b644c934780f06b
Anobaka <anobaka@qq.com>
2019-01-15 18:37:46 +0800

    update

--- Bootstrap.Infrastructures.Extensions/CryptographyUtils.cs
+++ Bootstrap.Infrastructures.Extensions/CryptographyUtils.cs
@@ -10,5 +10,29 @@ namespace Bootstrap.Infrastructures.Extensions
         {
             return BitConverter.ToString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-", "");
         }
+
+        public static byte[] Crc16(byte[] data)
+        {
+            var len = data.Length;
+            if (len > 0)
+            {
+                ushort crc = 0xFFFF;
+                for (var i = 0; i < len; i++)
+                {
+                    crc = (ushort) (crc ^ (data[i]));
+                    for (var j = 0; j < 8; j++)
+                    {
+                        crc = (crc & 1) != 0 ? (ushort) ((crc >> 1) ^ 0xA001) : (ushort) (crc >> 1);
+                    }
+                }
+
+                var hi = (byte) ((crc & 0xFF00) >> 8); //High
+                var lo = (byte) (crc & 0x00FF); //Low
+
+                return new[] {lo, hi};
+            }
+
+            return new byte[] {0, 0};
+        }
     }
 }
