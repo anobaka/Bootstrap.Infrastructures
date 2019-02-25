1450e9743f3e1aca2b933e1026eebbc7ffd98956
Anobaka <anobaka@qq.com>
2019-02-25 18:06:30 +0800

    update

--- Bootstrap.Infrastructures.Models/MultilevelData.cs
+++ Bootstrap.Infrastructures.Models/MultilevelData.cs
@@ -7,18 +7,18 @@ namespace Bootstrap.Infrastructures.Models
 {
     public abstract class MultilevelData<TData> where TData : MultilevelData<TData>
     {
-        [Key, Column("id")] public virtual int Id { get; set; }
+        [Key] public virtual int Id { get; set; }
 
-        [Required] [Column("name")] public virtual string Name { get; set; }
+        [Required] public virtual string Name { get; set; }
 
         [NotMapped] public virtual List<TData> Children { get; set; }
 
         [NotMapped] public virtual TData Parent { get; set; }
-        [Column("parent_id")] public virtual int? ParentId { get; set; }
-        [Column("left")] public virtual int Left { get; set; }
-        [Column("right")] public virtual int Right { get; set; }
+        public virtual int? ParentId { get; set; }
+        public virtual int Left { get; set; }
+        public virtual int Right { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity), Column("create_dt")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual DateTime CreateDt { get; set; }
 
         public virtual bool IsLeaf => Right - Left <= 1;
