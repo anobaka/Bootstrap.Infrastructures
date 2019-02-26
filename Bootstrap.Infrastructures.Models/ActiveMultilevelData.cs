009a1db0ca42510256a4731921c3435a373faf5f
Anobaka <anobaka@qq.com>
2019-02-26 11:27:33 +0800

    update

---
+++ Bootstrap.Infrastructures.Models/ActiveMultilevelData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bootstrap.Infrastructures.Models
+{
+    public abstract class ActiveMultilevelData<TData> : MultilevelData<TData> where TData : ActiveMultilevelData<TData>
+    {
+        public bool Active { get; set; }
+    }
+}
