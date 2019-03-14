4d6b852468562f2f11e6a77f5c4fb3c898c59110
Anobaka <anobaka@qq.com>
2019-03-14 14:41:47 +0800

    update

--- Bootstrap.Infrastructures.Extensions/CryptographyUtils.cs
+++ Bootstrap.Infrastructures.Extensions/CryptographyUtils.cs
@@ -34,5 +34,47 @@ namespace Bootstrap.Infrastructures.Extensions
 
             return new byte[] {0, 0};
         }
+
+        public static string AesEncrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            var toEncryptArray = Encoding.UTF8.GetBytes(str);
+
+            var rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            var cTransform = rm.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        ///  AES 解密
+        /// </summary>
+        /// <param name="str">明文（待解密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesDecrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            var toEncryptArray = Convert.FromBase64String(str);
+
+            var rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            var cTransform = rm.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
     }
 }
