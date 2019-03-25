27cbd072ffcb3e42566074f09050d5ec58a33101
Anobaka <anobaka@qq.com>
2019-03-25 15:02:08 +0800

    update

--- Bootstrap.Infrastructures.Extensions/SpecificTypeUtils.cs
+++ Bootstrap.Infrastructures.Extensions/SpecificTypeUtils.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Bootstrap.Infrastructures.Extensions
 {
     public class SpecificTypeUtils<T>
     {
         public static Type Type = typeof(T);
+        public static TypeInfo TypeInfo = Type.GetTypeInfo();
+
+        public static bool IsTypeOfNullable() => TypeInfo.IsTypeOfNullable();
     }
 }
---
+++ Bootstrap.Infrastructures.Extensions/TypeExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Bootstrap.Infrastructures.Extensions
+{
+    public static class TypeExtensions
+    {
+        private static readonly ConcurrentDictionary<Type, TypeInfo> Cache = new ConcurrentDictionary<Type, TypeInfo>();
+
+        public static bool IsTypeOfNullable(this TypeInfo typeInfo)
+        {
+            return typeInfo.IsGenericType && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        public static TypeInfo GetTypeInfo(this Type type)
+        {
+            return Cache.GetOrAdd(type, t => t.GetTypeInfo());
+        }
+
+        public static bool IsTypeOfNullable(this Type type)
+        {
+            return type.GetTypeInfo().IsTypeOfNullable();
+        }
+
+        public static Type GetMostBaseClassOfNullableType(this Type type)
+        {
+            var t = type;
+            while (t.IsTypeOfNullable())
+            {
+                t = t.GetGenericArguments()[0];
+            }
+
+            return t;
+        }
+
+        public static bool IsSimple(this Type type)
+        {
+            var t = type.GetMostBaseClassOfNullableType();
+            var typeInfo = t.GetTypeInfo();
+            return typeInfo.IsPrimitive || typeInfo.IsEnum || type == typeof(string) || type == typeof(decimal);
+        }
+    }
+}
