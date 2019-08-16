672c68c898f620ef319dbe6e43fc05e44617c4c9
Jax <jax.zhu@aihuishou.com>
2019-08-16 17:13:00 +0800

    update

---
+++ Bootstrap.Infrastructures.Extensions/QuartzExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Bootstrap.Infrastructures.Extensions
+{
+    public static class QuartzExtensions
+    {
+        public static T GetData<T>(this IJobExecutionContext context)
+        {
+            return (T) context.MergedJobDataMap.FirstOrDefault(t => t is T).Value;
+        }
+
+        public static IEnumerable<T> GetAllData<T>(this IJobExecutionContext context)
+        {
+            return context.MergedJobDataMap.Where(t => t.Value is T).Select(t => (T) t.Value);
+        }
+
+        public static T GetData<T>(this IJobExecutionContext context, string key)
+        {
+            return (T) context.MergedJobDataMap[key];
+        }
+    }
+}
