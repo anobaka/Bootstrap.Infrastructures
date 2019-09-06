9076bbfc0cdfce66f8da089d1248acc425dc0727
Jax <jax.zhu@aihuishou.com>
2019-09-06 18:20:44 +0800

    update

---
+++ Bootstrap.Infrastructures.Extensions/FixSessionIdAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Bootstrap.Infrastructures.Extensions
+{
+    public class FixSessionIdAttribute : ActionFilterAttribute
+    {
+        public FixSessionIdAttribute()
+        {
+            Order = int.MinValue;
+        }
+
+        private const string InitSessionKey = "Initialized";
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Session.GetInt32(InitSessionKey).HasValue)
+            {
+                context.HttpContext.Session.SetInt32(InitSessionKey, 1);
+            }
+
+            await base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
---
+++ Bootstrap.Infrastructures.Extensions/SessionTicketManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Bootstrap.Infrastructures.Extensions
+{
+    /// <summary>
+    /// 每一种【安全验证等级（如短息验证、密码验证）】可对应一系列Token，Token可能用于：修改密码，支付等目标；可根据需求更换数据源
+    /// </summary>
+    public abstract class SessionTicketManager<TTicketPurpose> where TTicketPurpose : Enum
+    {
+        private class Ticket
+        {
+            public TTicketPurpose Purpose { get; set; }
+            public DateTimeOffset? ExpireDt { get; set; }
+        }
+
+        private readonly IHttpContextAccessor _accessor;
+        private const string KeyPrefix = "Ticket-";
+
+        protected HttpContext HttpContext => _accessor.HttpContext;
+
+        protected SessionTicketManager(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public Task AddToken(TTicketPurpose purpose, DateTimeOffset? expireDt)
+        {
+            HttpContext.Session.Set($"{KeyPrefix}{purpose}", new Ticket {Purpose = purpose, ExpireDt = expireDt});
+            return Task.CompletedTask;
+        }
+
+        public Task DisableToken(TTicketPurpose purpose)
+        {
+            HttpContext.Session.Remove($"{KeyPrefix}{purpose}");
+            return Task.CompletedTask;
+        }
+
+        public async Task<bool> ValidateToken(TTicketPurpose purpose, bool consume = true)
+        {
+            var token = HttpContext.Session.Get<Ticket>($"{KeyPrefix}{purpose}");
+            var success = false;
+            if (token != null)
+            {
+                if (token.ExpireDt.HasValue || token.ExpireDt > DateTimeOffset.Now)
+                {
+                    success = true;
+                }
+
+                if (consume)
+                {
+                    await DisableToken(purpose);
+                }
+            }
+
+            return success;
+        }
+    }
+}
